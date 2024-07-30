940b68f62ef6fd4c99221ae2272634a4fe3db050
shubhamsaklani <54273868+shubhamsaklani@users.noreply.github.com>
2024-07-30 23:39:07 +0530

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+# Initialize Flask application
+app = Flask(__name__)
+
+knn_model = joblib.load('../knn_model.joblib')  # Adjust the path if needed
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = knn_model.predict(features)
+    response = {
+        'prediction': int(prediction[0])
+    }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
